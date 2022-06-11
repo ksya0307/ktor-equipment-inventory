88b91b0369402ac2434bc94e819c7673957f8f83
ksya0307 <alexeeva.ksjusch@yandex.ru>
2022-06-11 17:18:53 +0300

    repair date time

--- src/main/kotlin/com/ksenialexeev/models/RepairDto.kt
+++ src/main/kotlin/com/ksenialexeev/models/RepairDto.kt
@@ -1,14 +1,17 @@
 package com.ksenialexeev.models
 
+import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.Serializable
 
 
 @Serializable
-data class RepairDto(val phone:String)
+data class RepairDto(val phone:String, val datetime: LocalDate? = Clock.System.now().toLocalDateTime(TimeZone.UTC).date)
 
 @Serializable
-data class UpdateRepairDto(val phone:String, val datetime: LocalDate,val completed:Boolean)
+data class UpdateRepairDto(val id:Int,val phone:String, val datetime: LocalDate,val completed:Boolean)
 
 @Serializable
 data class RepairEquipmentDto(val repair: RepairDto, val equipment: ClassroomEquipmentDto?,val problem:String)
